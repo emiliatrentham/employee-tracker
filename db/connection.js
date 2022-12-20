0bdb92de7433ac733f7adc3f45c63d808c1db0a0
Emilia Trentham <lavernatrentham@gmail.com>
2022-12-20 14:12:05 -0800

    Create Connection

--- db/connection.js
+++ db/connection.js
@@ -8,12 +8,8 @@ const db = mysql.createConnection(
       password: '',
       database: 'employee_db'
     },
-    console.log(`Connected to the employee_db database.`)
 );
-  
+
 module.exports = db;
-  
-// inquirer part set up interface fr when we star the app 
-// depending on what user picks use mysql package to query rows 
-// look at activities 21 and 22 db.query
+
 
